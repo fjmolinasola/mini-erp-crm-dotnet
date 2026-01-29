b5c75e3b72592d8364ee4dedca43b8fa725e3ca1
fjmolinasola <franciscojosemolinasola@gmail.com>
2026-01-29 13:06:08 +0100

    Setup ASP.NET Core Web API with Swagger and health endpoint

---
+++ src/MiniErpCrm/MiniErpCrm.Api/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniErpCrm.Api.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { status = "OK" });
+    }
+}
